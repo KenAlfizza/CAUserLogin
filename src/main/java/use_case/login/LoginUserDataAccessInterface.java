c21b7adfac561fc9f238477d03c7bc1c3638b9c7
KenAlfizza <kenazalfizza@Outlook.com>
2024-10-21 01:45:50 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the current user of the app.
+     * @param name the username to set
+     */
+    void setCurrentUser(String name);
 }
